be180ad48bb34d6c6751b0c9465f1d7cc9a0ae3e
Artur Rodrigues <arturhoo@gmail.com>
2023-10-22 19:14:10 +0100

    Ratelimiting impl

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
+
+	"golang.org/x/time/rate"
 
 	"github.com/gin-gonic/gin"
+	admissionv1 "k8s.io/api/admission/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.POST("/validate", validatingHandler)
 	r.GET("/healthz", healthzHandler)
 	r.GET("/livez", livezHandler)
 
@@ -37,6 +43,34 @@ func main() {
 	} else {
 		r.Run(addr)
 	}
+}
+
+var limiter = rate.NewLimiter(rate.Every(10*time.Second), 1)
+
+func validatingHandler(c *gin.Context) {
+	var review admissionv1.AdmissionReview
+	if err := c.Bind(&review); err != nil {
+		return
+	}
+
+	allowed := limiter.Allow()
+	var status, msg string
+	if allowed {
+		status = metav1.StatusSuccess
+	} else {
+		status = metav1.StatusFailure
+		msg = "rate limit exceeded"
+	}
+
+	review.Response = &admissionv1.AdmissionResponse{
+		UID:     review.Request.UID,
+		Allowed: allowed,
+		Result: &metav1.Status{
+			Status:  status,
+			Message: msg,
+		},
+	}
+	c.JSON(200, review)
 }
 
 func healthzHandler(c *gin.Context) {
