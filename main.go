33fc753fbeb3bb0239a1dc32d13b822392e6087e
Artur Rodrigues <arturhoo@gmail.com>
2023-10-22 07:40:54 +0100

    Parametrize host:port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -12,15 +13,30 @@ func main() {
 	r.GET("/healthz", healthzHandler)
 	r.GET("/livez", livezHandler)
 
-	var certPath, keyPath string
-	if certPath = os.Getenv("CERT_PATH"); certPath == "" {
-		certPath = "./cert/localhost.pem"
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	tlsEnabled := os.Getenv("TLS_ENABLED")
+	if port == "" {
+		if tlsEnabled == "true" {
+			port = "8443"
+		} else {
+			port = "8080"
+		}
 	}
-	if keyPath = os.Getenv("KEY_PATH"); keyPath == "" {
-		keyPath = "./cert/localhost-key.pem"
-	}
+	addr := fmt.Sprintf("%s:%s", host, port)
 
-	r.RunTLS("localhost:8443", certPath, keyPath)
+	if tlsEnabled == "true" {
+		var certPath, keyPath string
+		if certPath = os.Getenv("CERT_PATH"); certPath == "" {
+			certPath = "./cert/localhost.pem"
+		}
+		if keyPath = os.Getenv("KEY_PATH"); keyPath == "" {
+			keyPath = "./cert/localhost-key.pem"
+		}
+		r.RunTLS(addr, certPath, keyPath)
+	} else {
+		r.Run(addr)
+	}
 }
 
 func healthzHandler(c *gin.Context) {
